54f9c01e62ad803a203717bb65d1461f8c52ea2c
Masaki Muranaka <monaka@monami-ya.com>
2016-11-08 01:51:32 +0000

    .

---
+++ server.js
@@ -0,0 +1,20 @@
+var http      = require('http');
+var httpProxy = require('http-proxy');
+
+var proxy = httpProxy.createProxyServer({
+	target: {
+		host: 'eclipse-che.default.svc.cluster.local',
+		port: 8080
+	}
+});
+
+var proxyServer = http.createServer(function (req, res) {
+	req.headers['Host'] = 'localhost';
+	proxy.web(req, res);
+});
+
+proxyServer.on('upgrade', function(req, socket, head) {
+	proxy.ws(req, socket, head);
+});
+
+proxyServer.listen(process.env.PORT || 5000);
