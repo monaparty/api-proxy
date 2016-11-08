d1a1629a0b78ba76e99647f674dc13647cf00fe3
Masaki Muranaka <monaka@monami-ya.com>
2016-11-08 03:00:28 +0000

    .

--- server.js
+++ server.js
@@ -16,4 +16,10 @@ proxyServer.on('upgrade', function(req, socket, head) {
 	proxy.ws(req, socket, head);
 });
 
+/* Avoid CORS. */
+proxyServer.on('proxyRes', function(proxyRes, req, res) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+});
+
 proxyServer.listen(process.env.PORT || 5000);
