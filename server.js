5a997895aacb49a9abae7acd7ca176a2a5ac2fb7
Masaki Muranaka <monaka@monami-ya.com>
2016-11-11 06:54:24 +0000

    Fix CORS.

--- server.js
+++ server.js
@@ -19,7 +19,14 @@ proxyServer.on('upgrade', function(req, socket, head) {
 /* Avoid CORS. */
 proxyServer.on('proxyRes', function(proxyRes, req, res) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'authorization, content-type');
+	if (req.method === 'OPTIONS') {
+		res.writeHead(200);
+		res.end();
+		return;
+	}
 });
 
 proxyServer.listen(process.env.PORT || 5000);
